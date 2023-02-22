ce40b8f58e3d94de18d77eede7350379b5141450
Azizbek Olimov <azizolimwow@gmail.com>
2023-02-22 23:01:17 +0100

    main panel features added

--- Assets/Scripts/UI/CanvasController.cs
+++ Assets/Scripts/UI/CanvasController.cs
@@ -1,46 +1,40 @@
 using System;
 using UI;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class CanvasController : MonoBehaviour
 {
     [SerializeField] private CanvasGroup scrollPanel;
-    [SerializeField] private CanvasGroup mainPanel;
-    [SerializeField] private CanvasGroup shopPanel;
-    [SerializeField] private CanvasGroup buttonsPanel;
     [SerializeField] private Button scrollButton;
-    [SerializeField] private Button shopButton;
-    [SerializeField] private Button onMainPanelButton;
+    [SerializeField] private CanvasGroup mainPanel;
+    [SerializeField] private Button playButton;
+    
     private ChangePanel _changePanel;
 
     private void Start()
     {
         _changePanel = new ChangePanel();
         OnMainPanel();
-        ButtonsPanel();
         scrollButton.onClick.AddListener(OnScrollPanel);
-        shopButton.onClick.AddListener(OnShopPAnel);
-        onMainPanelButton.onClick.AddListener(OnMainPanel);
+        playButton.onClick.AddListener(OnPlayButton);
+        
     }
-
-    public void ButtonsPanel()
-    {
-        _changePanel.SetPanel(buttonsPanel);
-    }
-
-    public void OnMainPanel()
-    {
-        _changePanel.SetPanel(mainPanel);
-    }
-
+    
     public void OnScrollPanel()
     {
         _changePanel.SetPanel(scrollPanel);
     }
 
-    public void OnShopPAnel()
+    void OnMainPanel()
     {
-        _changePanel.SetPanel(shopPanel);
+        _changePanel.SetPanel(mainPanel);
     }
+
+    void OnPlayButton()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
 }
